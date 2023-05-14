d0ae0c209f51a5f0c0cd0d6acd37e46bca6fc626
ForexPro <roma.minihanowa@mail.ru>
2023-05-15 00:26:50 +0400

    ОНО НЕ ПАДАЕТ!!!

--- src/main/java/com/example/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/example/chat/repository/ChatMessageRepository.java
@@ -3,7 +3,10 @@ package com.example.chat.repository;
 
 import com.example.chat.model.ChatMessage;
 import com.example.chat.model.MessageStatus;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +15,17 @@ import java.util.UUID;
 @Repository
 public interface ChatMessageRepository extends CrudRepository<ChatMessage, UUID> {
 
-    long countBySenderUuidAndRecipientUuidAndStatus(UUID senderId, UUID recipientId, MessageStatus status);
+    long countBySenderUuidAndRecipientUuidAndStatus(UUID senderUuid, UUID recipientId, MessageStatus status);
 //    void update
 
-    List<ChatMessage> findByChatId(String chatId);
+    List<ChatMessage> findByChatUuid(String chatUuid);
+
+    @Modifying
+    @Query("update ChatMessage m set m.status = :status " +
+            "where m.senderUuid = :senderUuid " +
+            "and  m.recipientUuid = :recipientUuid")
+    void updateChatMessageStatus(
+            @Param(value = "senderUuid") UUID senderUuid,
+            @Param(value = "recipientUuid") UUID recipientUuid,
+            @Param(value = "status") MessageStatus status);
 }
