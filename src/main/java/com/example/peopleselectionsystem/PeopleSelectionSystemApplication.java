c5ebdb24f34b5e130a39e0dd122c635a69f4de3a
ForexPro <roma.minihanowa@mail.ru>
2023-05-17 01:27:41 +0400

    Улучшения по сохранению файлов

--- src/main/java/com/example/peopleselectionsystem/PeopleSelectionSystemApplication.java
+++ src/main/java/com/example/peopleselectionsystem/PeopleSelectionSystemApplication.java
@@ -1,13 +1,17 @@
 package com.example.peopleselectionsystem;
 
+import com.example.storage.service.StorageService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -18,6 +22,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 public class PeopleSelectionSystemApplication {
 
+    @Autowired
+    StorageService storageService;
+
+
     public static void main(String[] args) {
         SpringApplication.run(PeopleSelectionSystemApplication.class, args);
     }
@@ -25,6 +33,13 @@ public class PeopleSelectionSystemApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+        storageService.init();
+        System.err.println("Yaaah, I am running........");
     }
 
 }
---
+++ src/main/java/com/example/storage/service/FileStorageService.java
@@ -0,0 +1,84 @@
+package com.example.storage.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+@Service
+public class FileStorageService implements StorageService {
+    private Path root;
+
+    public FileStorageService(@Value("${filepath:uploads}") String text) {
+        this.root = Paths.get(text);
+    }
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize folder for upload!");
+        }
+    }
+
+    @Override
+    public void save(MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+        } catch (Exception e) {
+            if (e instanceof FileAlreadyExistsException) {
+                throw new RuntimeException("A file of that name already exists.");
+            }
+
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void save(MultipartFile file, String filename) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load the files!");
+        }
+    }
+}
