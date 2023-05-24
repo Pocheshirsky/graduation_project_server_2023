a9e5e3a458d29c08c0c212e95c6f3a30af4df012
Osiris <pocheshirsky@gmail.com>
2023-05-24 16:15:11 +0400

    Подготовка Searching

--- src/main/java/com/example/searching/repository/UserPoolRepository.java
+++ src/main/java/com/example/searching/repository/UserPoolRepository.java
@@ -5,6 +5,13 @@ import com.example.searching.model.UserPool;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -13,4 +20,6 @@ public interface UserPoolRepository extends CrudRepository<UserPool, UUID> {
     boolean existsByUserInfoUuid(UUID userInfoUuid);
 
     void deleteByUserInfoUuid(UUID userInfoUuid);
+
+
 }
